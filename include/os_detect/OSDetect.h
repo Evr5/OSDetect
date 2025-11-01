7e160182d62b9bde3e85c7601e8d60b32044cc5d
Ethan Van Ruyskensvelde <ethanvr3@gmail.com>
2025-11-01 13:37:50 +0100

    clean code + doc

--- include/os_detect/OSDetect.h
+++ include/os_detect/OSDetect.h
@@ -25,7 +25,8 @@ namespace OSDetect {
         Monterey,
         Ventura, 
         Sonoma,  
-        Sequoia, 
+        Sequoia,
+        Tahoe,
         Older,
         Unknown
     };
@@ -40,19 +41,48 @@ namespace OSDetect {
         CentOS,
         RedHat,
         Alpine,
+        NixOS,
         Unknown
     };
 
+    /**
+     * @brief Return the basic os where the program is running. Linux, macOS or Windows
+     * 
+     * @return OS the os where the program is running
+     */
     OS getOS();
 
+    /**
+     * @brief Get the Windows Version object
+     * 
+     * @return WindowsVersion 
+     */
     WindowsVersion getWindowsVersion();
+    /**
+     * @brief Get the MacOS Version object
+     * 
+     * @return MacOSVersion 
+     */
     MacOSVersion getMacOSVersion();
+    /**
+     * @brief Get the Linux Distro object
+     * 
+     * @return LinuxDistro 
+     */
     LinuxDistro getLinuxDistro();
 
-    // --- Interface générique basée sur template ---
+    /**
+     * @brief Get the Detailed OS object, more specific
+     * 
+     * @tparam T 
+     * @return T 
+     */
     template <typename T>
     T getDetailedOS();
 
+    // ------------------------------
+    // Flow operator overload (<<)
+    // ------------------------------
     std::ostream& operator<<(std::ostream& os, OSDetect::OS value);
     std::ostream& operator<<(std::ostream& os, OSDetect::WindowsVersion value);
     std::ostream& operator<<(std::ostream& os, OSDetect::MacOSVersion value);
--- src/OSDetect.cpp
+++ src/OSDetect.cpp
@@ -29,74 +29,75 @@ namespace OSDetect {
     // Windows
     // ------------------------------
     WindowsVersion getWindowsVersion() {
-    #if defined(_WIN32)
-        if (IsWindows10OrGreater()) return WindowsVersion::Windows10;
-        if (IsWindows8OrGreater())  return WindowsVersion::Windows8;
-        if (IsWindows7OrGreater())  return WindowsVersion::Windows7;
-        return WindowsVersion::Older;
-    #else
-        return WindowsVersion::Unknown;
-    #endif
+        #if defined(_WIN32)
+            if (IsWindows10OrGreater()) return WindowsVersion::Windows10;
+            if (IsWindows8OrGreater())  return WindowsVersion::Windows8;
+            if (IsWindows7OrGreater())  return WindowsVersion::Windows7;
+            return WindowsVersion::Older;
+        #else
+            return WindowsVersion::Unknown;
+        #endif
     }
 
     // ------------------------------
     // macOS
     // ------------------------------
     MacOSVersion getMacOSVersion() {
-    #if defined(__APPLE__) || defined(__MACH__)
-        struct utsname buffer;
-        if (uname(&buffer) == 0) {
-            std::string rel = buffer.release;
-            // macOS 10.15+ : 19.x = Catalina, 20.x = Big Sur, 21.x = Monterey, 22.x = Ventura, 23.x = Sonoma, 24.x = Sequoia
-            int major = std::stoi(rel.substr(0, rel.find('.')));
-            switch (major) {
-                case 19: return MacOSVersion::Catalina;
-                case 20: return MacOSVersion::BigSur;
-                case 21: return MacOSVersion::Monterey;
-                case 22: return MacOSVersion::Ventura;
-                case 23: return MacOSVersion::Sonoma;
-                case 24: return MacOSVersion::Sequoia;
-                default: return MacOSVersion::Older;
+        #if defined(__APPLE__) || defined(__MACH__)
+            struct utsname buffer;
+            if (uname(&buffer) == 0) {
+                std::string rel = buffer.release;
+                int major = std::stoi(rel.substr(0, rel.find('.')));
+                switch (major) {
+                    case 19: return MacOSVersion::Catalina;
+                    case 20: return MacOSVersion::BigSur;
+                    case 21: return MacOSVersion::Monterey;
+                    case 22: return MacOSVersion::Ventura;
+                    case 23: return MacOSVersion::Sonoma;
+                    case 24: return MacOSVersion::Sequoia;
+                    case 25: return MacOSVersion::Tahoe
+                    default: return MacOSVersion::Older;
+                }
             }
-        }
-        return MacOSVersion::Unknown;
-    #else
-        return MacOSVersion::Unknown;
-    #endif
+            return MacOSVersion::Unknown;
+        #else
+            return MacOSVersion::Unknown;
+        #endif
     }
 
     // ------------------------------
     // Linux
     // ------------------------------
     LinuxDistro getLinuxDistro() {
-    #if defined(__linux__)
-        std::ifstream file("/etc/os-release");
-        if (file) {
-            std::string line;
-            while (std::getline(file, line)) {
-                if (line.find("ID=") == 0) {
-                    std::string id = line.substr(3);
-                    if (id == "ubuntu")  return LinuxDistro::Ubuntu;
-                    if (id == "debian")  return LinuxDistro::Debian;
-                    if (id == "fedora")  return LinuxDistro::Fedora;
-                    if (id == "arch")    return LinuxDistro::Arch;
-                    if (id == "opensuse")return LinuxDistro::OpenSUSE;
-                    if (id == "manjaro") return LinuxDistro::Manjaro;
-                    if (id == "centos")  return LinuxDistro::CentOS;
-                    if (id == "rhel" || id == "redhat") return LinuxDistro::RedHat;
-                    if (id == "alpine")  return LinuxDistro::Alpine;
-                    return LinuxDistro::Unknown;
+        #if defined(__linux__)
+            std::ifstream file("/etc/os-release");
+            if (file) {
+                std::string line;
+                while (std::getline(file, line)) {
+                    if (line.find("ID=") == 0) {
+                        std::string id = line.substr(3);
+                        if (id == "ubuntu")  return LinuxDistro::Ubuntu;
+                        if (id == "debian")  return LinuxDistro::Debian;
+                        if (id == "fedora")  return LinuxDistro::Fedora;
+                        if (id == "arch")    return LinuxDistro::Arch;
+                        if (id == "opensuse")return LinuxDistro::OpenSUSE;
+                        if (id == "manjaro") return LinuxDistro::Manjaro;
+                        if (id == "centos")  return LinuxDistro::CentOS;
+                        if (id == "rhel" || id == "redhat") return LinuxDistro::RedHat;
+                        if (id == "alpine")  return LinuxDistro::Alpine;
+                        if (id == "nixos")     return LinuxDistro::NixOS; 
+                        return LinuxDistro::Unknown;
+                    }
                 }
             }
-        }
-        return LinuxDistro::Unknown;
-    #else
-        return LinuxDistro::Unknown;
-    #endif
+            return LinuxDistro::Unknown;
+        #else
+            return LinuxDistro::Unknown;
+        #endif
     }
 
     // ------------------------------
-    // Template générique
+    // Template
     // ------------------------------
     template <>
     WindowsVersion getDetailedOS<WindowsVersion>() {
@@ -113,6 +114,9 @@ namespace OSDetect {
         return getLinuxDistro();
     }
 
+    // ------------------------------
+    // Flow operator overload (<<)
+    // ------------------------------
     std::ostream& operator<<(std::ostream& os, OS value) {
         switch (value) {
             case OS::Windows: return os << "Windows";
@@ -141,6 +145,7 @@ namespace OSDetect {
             case MacOSVersion::Ventura:  return os << "macOS Ventura (13)";
             case MacOSVersion::Sonoma:   return os << "macOS Sonoma (14)";
             case MacOSVersion::Sequoia:  return os << "macOS Sequoia (15)";
+            case MacOSVersion::Tahoe     return os << "macOS Tahoe (26)";
             case MacOSVersion::Older:    return os << "Older macOS";
             default:                     return os << "Unknown macOS Version";
         }
@@ -157,6 +162,7 @@ namespace OSDetect {
             case LinuxDistro::CentOS:   return os << "CentOS";
             case LinuxDistro::RedHat:   return os << "Red Hat";
             case LinuxDistro::Alpine:   return os << "Alpine Linux";
+            case LinuxDistro::NixOS:    return os << "NixOS";
             default:                    return os << "Unknown Linux Distro";
         }
     }
